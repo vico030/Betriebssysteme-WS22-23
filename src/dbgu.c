617ac0e80bace68c21e2509550c2eda2e4a7803e
marvellousmarvolo <la0gueule@gmail.com>
2022-11-05 13:01:32 +0100

    DBGU driver WIP

--- src/dbgu.c
+++ src/dbgu.c
@@ -11,3 +11,17 @@
 #define DBGU_RHR 0x0018 // Receive Holding Register Read-only 0x0
 #define DBGU_THR 0x001C // Transmit Holding Register Write-only –
 #define DBGU_BRGR 0x0020 // Baud Rate Generator Register Read/Write 0x0
+
+#define RSTRX 1 << 2 // Reset Receiver
+#define RSTTX 1 << 3 // Reset Transmitter
+#define RXEN 1 << 4 // Receiver Enable
+#define RXDIS 1 << 5 // Receiver Disable
+#define TXEN 1 << 6 // Transmitter Enable
+#define TXDIS 1 << 7 // Transmitter Disable
+#define RSTSTA 1 << 8 // Reset Status Bits
+
+static inline void write_u32(unsigned int addr, unsigned int val)
+{
+  *(volatile unsigned int *)addr = val;
+}
+
