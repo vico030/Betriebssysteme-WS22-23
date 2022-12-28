25e4d49629438dfa45a9c14eec613adde2882e39
marvellousmarvolo <la0gueule@gmail.com>
2022-12-28 20:28:00 +0100

    thread switching WIP: switch into second thread working, first is skipped, crash after return

--- src/sys/thread.c
+++ src/sys/thread.c
@@ -31,7 +31,7 @@ typedef struct {
 } tcb_container;
 
 
-tcb_container container; // Todo: potential boom
+tcb_container container;
 
 // TCB linked List
 
@@ -65,7 +65,7 @@ void create_thread(unsigned int function_ptr) {
   int slot = get_empty_tcb_slot();
   printf("found slot %d\r\n", slot);
   if (slot != -1) {
-    tcb* new_thread = &container.tcb_array[slot]; // Todo: potential boom
+    tcb* new_thread = &container.tcb_array[slot];
     new_thread->id = slot;
     new_thread->state = READY;
     new_thread->stack_pointer = BASE_ADDRESS - slot * STACK_SIZE;
@@ -73,15 +73,17 @@ void create_thread(unsigned int function_ptr) {
     container.tcb_count++;
 
     // push function pointer to SP
+    new_thread->stack_pointer -= 4;
     write_u32(new_thread->stack_pointer, function_ptr);
+
     // push r0 - r14 and SPSR to SP (in this case all 0) // Todo: potential boom - SPSR auf 0?
-    for (int i = 1; i < 17; i++){
-      write_u32(new_thread->stack_pointer - i, 0);
-    }
-    // set SP so all values can be popped on switch
-    new_thread->stack_pointer -= 17;
+//    for (int i = 1; i < 16; i++){  // Todo: 17 correct offset? 16?
+//      write_u32(new_thread->stack_pointer - i, 0);
+//    }
+//    // set SP so all values can be popped on switch
+//    new_thread->stack_pointer -= 16;
     printf("thread created\r\n");
-    printf("new thread state: %d, id: %d \r\n",new_thread->state, new_thread->id);
+    printf("new thread state: %d, id: %d, sp: 0x%x, fp: 0x%x \r\n",new_thread->state, new_thread->id, new_thread->stack_pointer, function_ptr);
   }
 }
 
@@ -94,19 +96,21 @@ void delete_thread() {
 
 void switch_thread() {
   // check for next ready/running thread
-  for (int i = (container.tcb_current_id + 1) & (THREAD_AMOUNT - 1);  // Todo: ID 0 is skipped!
-    i != container.tcb_current_id; // == instead != ???
+  for (int i = (container.tcb_current_id + 1) & (THREAD_AMOUNT - 1);  // Todo: current ID is skipped!
+    i != container.tcb_current_id;
     i = (i + 1) & (THREAD_AMOUNT - 1)) {
 
-    if (container.tcb_array[i].state == READY || container.tcb_array[i].state == RUNNING) {
-      printf(" TCB no. %d.\r\n", i);
+    enum status thread_state = container.tcb_array[i].state;
+
+    if (thread_state == READY || thread_state == RUNNING) {
+      printf("TCB no. %d.\r\n", i);
 
       // save context
-      tcb* current_thread = &container.tcb_array[container.tcb_current_id];
+      tcb* current_thread = &container.tcb_array[container.tcb_current_id]; // Todo: -1 not possible!!!!!!!
 
       // load context
       tcb* next_thread = &container.tcb_array[i];
-      printf("Still alive...\r\n");
+      printf("Still alive 1...\r\n");
 
       asm volatile(
           "STMFD SP!, {r14} \n\t" // link register for interrupt
@@ -120,19 +124,32 @@ void switch_thread() {
 
           "STR SP, %[current_sp] \n\t" // pcb->cpu_state = SP,   r0 = external c variable
           "LDR SP, %[next_sp] \n\t" // SP = next_pcb->cpu_state
-
-          // restore context
-          "LDMFD SP!, {r2} \n\t" // CPU state to R2
-          "MSR spsr, r2 \n\t" // and then into saved state
-          "LDMFD SP!, {r0-r14}^ \n\t" // user registers
-          "LDMFD SP!, {pc} \n\t" // link register for return from interrupt
           : [current_sp] "=&o" (current_thread->stack_pointer)
           : [next_sp] "o" (next_thread->stack_pointer)
-          :"sp", "lr");
+          :);
 
-      printf("Still alive...\r\n");
+
       // set container->tcb_current_id
       container.tcb_current_id = i;
+
+      if (thread_state == RUNNING) {
+        printf("Thread is in running state");
+        asm volatile(
+          // restore context
+            "LDMFD SP!, {r2} \n\t" // CPU state to R2
+            "MSR spsr, r2 \n\t" // and then into saved state
+            "LDMFD SP!, {r0-r14}^ \n\t" // user registers
+            "LDMFD SP!, {pc} \n\t" // link register for return from interrupt
+            );
+      } else if (thread_state == READY) {
+        printf("Thread is in ready state");
+
+        asm volatile(
+            "POP {PC}"
+            );
+      }
+
+      printf("Still alive 2...\r\n"); // Todo: CRASH
       // set current_thread->tcb_state RUNNING
       // print new line
       printf("\r\n");
